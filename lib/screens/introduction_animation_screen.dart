0c3b6f07a02b58dd1a7a50368f802db79ce0fe4a
Himanshu-AT <aristech.official@gmail.com>
2024-04-05 16:53:12 +0530

    generate animated welcome Screen

---
+++ lib/screens/introduction_animation_screen.dart
@@ -0,0 +1,123 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+import 'package:relax/screens/home_screen.dart';
+import 'package:relax/widgets/splash_screens.dart/care_view.dart';
+import 'package:relax/widgets/splash_screens.dart/center_next_button.dart';
+import 'package:relax/widgets/splash_screens.dart/mood_diary_vew.dart';
+import 'package:relax/widgets/splash_screens.dart/relax_view.dart';
+import 'package:relax/widgets/splash_screens.dart/splash_view.dart';
+import 'package:relax/widgets/splash_screens.dart/top_back_skip_view.dart';
+import 'package:relax/widgets/splash_screens.dart/welcome_view.dart';
+
+class IntroductionAnimationScreen extends StatefulWidget {
+  const IntroductionAnimationScreen({super.key});
+
+  @override
+  _IntroductionAnimationScreenState createState() =>
+      _IntroductionAnimationScreenState();
+}
+
+class _IntroductionAnimationScreenState
+    extends State<IntroductionAnimationScreen> with TickerProviderStateMixin {
+  AnimationController? _animationController;
+
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 8));
+    _animationController?.animateTo(0.0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(_animationController?.value);
+    return Scaffold(
+      backgroundColor: const Color(0xffF7EBE1),
+      body: ClipRect(
+        child: Stack(
+          children: [
+            SplashView(
+              animationController: _animationController!,
+            ),
+            RelaxView(
+              animationController: _animationController!,
+            ),
+            CareView(
+              animationController: _animationController!,
+            ),
+            MoodDiaryVew(
+              animationController: _animationController!,
+            ),
+            WelcomeView(
+              animationController: _animationController!,
+            ),
+            TopBackSkipView(
+              onBackClick: _onBackClick,
+              onSkipClick: _onSkipClick,
+              animationController: _animationController!,
+            ),
+            CenterNextButton(
+              animationController: _animationController!,
+              onNextClick: _onNextClick,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onSkipClick() {
+    _animationController?.animateTo(0.8,
+        duration: const Duration(milliseconds: 1200));
+  }
+
+  void _onBackClick() {
+    if (_animationController!.value >= 0 &&
+        _animationController!.value <= 0.2) {
+      _animationController?.animateTo(0.0);
+    } else if (_animationController!.value > 0.2 &&
+        _animationController!.value <= 0.4) {
+      _animationController?.animateTo(0.2);
+    } else if (_animationController!.value > 0.4 &&
+        _animationController!.value <= 0.6) {
+      _animationController?.animateTo(0.4);
+    } else if (_animationController!.value > 0.6 &&
+        _animationController!.value <= 0.8) {
+      _animationController?.animateTo(0.6);
+    } else if (_animationController!.value > 0.8 &&
+        _animationController!.value <= 1.0) {
+      _animationController?.animateTo(0.8);
+    }
+  }
+
+  void _onNextClick() {
+    if (_animationController!.value >= 0 &&
+        _animationController!.value <= 0.2) {
+      _animationController?.animateTo(0.4);
+    } else if (_animationController!.value > 0.2 &&
+        _animationController!.value <= 0.4) {
+      _animationController?.animateTo(0.6);
+    } else if (_animationController!.value > 0.4 &&
+        _animationController!.value <= 0.6) {
+      _animationController?.animateTo(0.8);
+    } else if (_animationController!.value > 0.6 &&
+        _animationController!.value <= 0.8) {
+      _signUpClick();
+    }
+  }
+
+  void _signUpClick() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const FitnessAppHomeScreen()),
+    );
+  }
+}
